cb64e8aec5e3139900f1062abbe778fd4d782aac
Igor Loredo <igorloredo12@gmail.com>
2025-11-28 01:27:10 -0300

    Add initial project structure with Maven configuration and Spring Boot setup

---
+++ src/main/java/com/example/THA/ThaApplication.java
@@ -0,0 +1,13 @@
+package com.example.THA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/THA/ThaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.THA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
